8752ffd355b8dc1ca9b8bdaea1c0356868d11f4e
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-21 16:20:42 +0300

    Proje dosyası ekle.

---
+++ DisicionStructure/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisicionStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //&& operatörü ile || operatörü arasındaki farkı if-else yapısı ile örneklendirelim
+
+            int sifre;
+            string kullanici;
+
+            Console.WriteLine("Lütfen kullanici adi giriniz : ");
+            kullanici = Console.ReadLine();
+            Console.WriteLine("Lütfen sifre giriniz : ");
+            sifre = Convert.ToInt32(Console.ReadLine());
+
+
+            // *** && kullanımi ***
+            //if (kullanici == "admin" && sifre == 123456)
+            //{
+            //    Console.WriteLine("Hosgeldiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("hata");
+            //}
+
+            //*** || kullanimi ***
+
+            if (kullanici == "admin" || sifre == 123456)
+            {
+                Console.WriteLine("Hosgeldiniz");
+            }
+            else
+            {
+                Console.WriteLine("hata");
+            }
+
+
+            Console.Read();
+        }
+    }
+}
